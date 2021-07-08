967c15b5ce1df732d0769e01aefd7b7fa34189a4
Mike Morton <mmorton7295@gmail.com>
2021-07-08 13:25:38 -0500

    adds validations to Meeting and Speaker models

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,9 @@
 class Meeting < ApplicationRecord
   has_many :meeting_speakers
-  has_many :speakers, through: :meeting_speakers
-  
+  has_many :speakers, through: :meeting_speakers 
+  validates :title, length: {minimum: 2}
+  validates :agenda, length: {minimum: 8}
+  validates :location, length: {minimum: 2}
+  validates :time, presence: true
+
 end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,5 +1,8 @@
 class Speaker < ApplicationRecord
   has_many :meeting_speakers
   has_many :meetings, through: :meeting_speakers
-  
+  validates :first_name, length: {minimum: 2}
+  validates :last_name, length: {minimum: 2}
+  validates :email, format: {with: /\A(.+)@(.+)\z/, message: "Email invalid"}, uniqueness: true
+
 end
